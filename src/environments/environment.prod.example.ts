b3bb1dbe89fa60a43b08a3235e40ca1ac0376929
Matheus Nascimento Cavallini <nascimentocavallini@hotmail.com>
2020-02-12 12:10:19 -0300

    added environment examples

---
+++ src/environments/environment.prod.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    baseUri: {
+      website: 'https://yourwebsite.com',
+      mongo: 'https://yourwebsite.com/rest-api',
+      uploadFile: 'https://yourwebsite.com/rest-api/files/create',
+      uploadImage: 'https://yourwebsite.com/rest-api/images/create'
+    },
+    keys: {
+      googleMaps: 'YOUR_GOOGLE_MAPS_API_KEY'
+    }
+  };
