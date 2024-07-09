0c37d06e3f68770c129b4f38368d400269c8b14d
Matheus Cavallini <nascimentocavallini@hotmail.com>
2024-07-09 13:46:42 -0300

    feat: Add UsefullService to FooterComponent constructor

--- src/app/b2c/components/footer/footer.component.ts
+++ src/app/b2c/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { UsefullService } from "src/app/services/usefull.service";
 
 @Component({
   selector: "app-footer",
@@ -8,7 +9,7 @@ import { Component, OnInit } from "@angular/core";
 export class FooterComponent implements OnInit {
   currentYear: number;
 
-  constructor() {
+  constructor(public usefullService: UsefullService) {
     this.currentYear = new Date().getFullYear();
   }
 
